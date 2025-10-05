6bd7c986b32ca8f8d7e3138e7e6adafbe218565f
Frederic20021 <frederic123.bf@gmail.com>
2025-10-05 11:12:44 +0900

    "removed unused import"

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ConditionalLayout from "./components/ConditionalLayout";
-import { Suspense } from 'react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
