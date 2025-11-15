33e8d84f951c6a7d3f671b852183c4275f4424be
Frederic20021 <frederic123.bf@gmail.com>
2025-11-16 05:40:20 +0900

    "reconfigured the files to work on vercel"

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,14 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? '/empowerandlink' : '';
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
-    unoptimized: true
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
-  basePath,
-  assetPrefix: basePath,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath
-  }
 };
 
 export default nextConfig;
